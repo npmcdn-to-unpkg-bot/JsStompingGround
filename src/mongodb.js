0efc18da92b7eb1726ce54e7559d26c13a5526cc
daniel-williams <mail@danielwilliams.us>
2015-12-07 19:36:59 -0800

    Import of alfa flat files.

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,5 +1,173 @@
 import fs from 'fs';
 
+var DATA_ROOT = './data/';
+
+var galleries = getGalleries();
+var orientations = getOrientations();
+var keywords = getKeywords();
+var categories = getLookupTable();
+var art = getArt();
+
+module.exports = {
+    test: function() {
+
+        console.log('total galleries:', galleries.length);
+        console.log('total orientations:', orientations.length);
+        console.log('total keywords:', keywords.length);
+        console.log('total categories:', categories.length)
+        console.log('total art:', art.length, art.filter(function(item) {
+            return (item.keywords.length > 0)
+        }));
+
+        fs.writeFile(DATA_ROOT + 'alfa-art.json', JSON.stringify(art));
+
+    },
+}
+
+
+function getGalleries() {
+    var galleries = [];
+    var data = fs.readFileSync(DATA_ROOT + 'alfa-galleries.txt', 'utf8');
+
+    var lines = data.split('\r\n').slice(1);
+    galleries = lines.reduce(function(accum, line, i) {
+        var fields = line.split('\t');
+        if(fields.length !== 4) return accum;
+
+        accum.push({
+            id: fields[0],
+            name: fields[1],
+            thumb: fields[2],
+            order: fields[3],
+        });
+        return accum;
+    }, []);
+    return galleries.sort(function(a, b) {
+        return a.order === b.order ? 0 : a.order < b.order ? -1 : 1;
+    });
+}
+
+function getOrientations() {
+    var orientation = [];
+    var data = fs.readFileSync(DATA_ROOT + 'alfa-orientation.txt', 'utf8');
+
+    var lines = data.split('\r\n').slice(1);
+    orientation = lines.reduce(function(accum, line, i) {
+        var fields = line.split('\t');
+        if(fields.length !== 2) return accum;
+
+        accum.push({
+            id: fields[0],
+            name: fields[1],
+        });
+        return accum;
+    }, []);
+    return orientation;
+}
+
+function getKeywords() {
+    var keywords = [];
+    var data = fs.readFileSync(DATA_ROOT + 'alfa-keywords.txt', 'utf8');
+
+    var lines = data.split('\r\n').slice(1);
+    keywords = lines.reduce(function(accum, line, i) {
+        var fields = line.split('\t');
+        if(fields.length !== 2) return accum;
+
+        accum.push({
+            id: fields[0],
+            name: fields[1],
+        });
+        return accum;
+    }, []);
+    return keywords;
+}
+
+function getLookupTable() {
+    var lookupTable = [];
+    var data = fs.readFileSync(DATA_ROOT + 'alfa-art-cat.txt', 'utf8');
+
+    var lines = data.split('\r\n').slice(1);
+    lookupTable = lines.reduce(function(accum, line, i) {
+        var fields = line.split('\t');
+        if(fields.length !== 2) return accum;
+
+        accum.push({
+            artId: fields[0],
+            catId: fields[1],
+        });
+        return accum;
+    }, []);
+    return lookupTable;
+}
+
+
+
+function getArt() {
+    var art = [];
+    try {
+        var data = fs.readFileSync(DATA_ROOT +'alfa_art.txt', 'utf8');
+        var lines = data.split('\r\n').slice(1);
+        art = lines.reduce(function(accum, line, i) {
+            var fields = line.split('\t');
+            if(fields.length < 14) return accum;
+            fields = fields.map(item => item === 'NULL' ? undefined : item);
+
+            var width = toInt(fields[7]);
+            var height = toInt(fields[8]);
+            accum.push({
+                id: fields[0],
+                title: fields[1],
+                filename: fields[2],
+                media: fields[3],
+                description: fields[4],
+                created: toDate(toInt(fields[6]), toInt(fields[5]) - 1),
+                width: width,
+                height: height,
+                isForSale: toBool(fields[9]),
+                price: toFloat(fields[10]),
+                isSold: toBool(fields[11]),
+                isActive: toBool(fields[12]),
+                galleries: buildGalleryList(fields[13]),
+                orientation: width > height ? ['horizontal']
+                                            : width < height ? ['vertical']
+                                                             : ['horizontal', 'vertical'],
+                keywords: buildKeywordList(fields[0]),
+            });
+            return accum;
+        }, []);
+    } catch(err) { console.log('Error reading art.', err); }
+
+    return art;
+}
+
+function buildGalleryList(id) {
+    var galleryList = [];
+    galleries.forEach(function(gallery, i) {
+        if(gallery.id === id) {
+            galleryList.push(gallery.name);
+        }
+    });
+    return galleryList;
+}
+
+function buildKeywordList(artId) {
+    var keywordList = [];
+    categories.forEach(function(cat) {
+        if(cat.artId === artId) {
+            keywords.forEach(function(keyword) {
+                if(keyword.id === cat.catId) {
+                    keywordList.push(keyword.name);
+                }
+            });
+        }
+    });
+    return keywordList;
+}
+
+
+
+
 function toBool(val) {
     return isNaN(parseInt(val)) ? !!val : !!parseInt(val);
 }
@@ -17,37 +185,4 @@ function toDate(year, month) {
         console.log('Error parsing date from: ', year, month);
     }
     return created;
-}
-
-module.exports = {
-    test: function() {
-        fs.readFile('./data/alfa_art.txt', 'utf8', function(err, data) {
-            var lines = data.split('\r\n');
-            var items = lines.slice(1).reduce(function(accum, line, i) {
-                var fields = line.split('\t');
-                if(fields.length < 14) return accum;
-                fields = fields.map(item => item === 'NULL' ? undefined : item);
-
-
-                accum.push({
-                    id: fields[0],
-                    title: fields[1],
-                    filename: fields[2],
-                    media: fields[3],
-                    description: fields[4],
-                    created: toDate(toInt(fields[6]), toInt(fields[5]) - 1),
-                    width: toInt(fields[7]),
-                    height: toInt(fields[8]),
-                    isForSale: toBool(fields[9]),
-                    price: toFloat(fields[10]),
-                    isSold: toBool(fields[11]),
-                    isActive: toBool(fields[12]),
-                    galleryId: fields[13],
-                });
-                return accum;
-            }, []);
-            console.log('total items:', items.length);
-            console.log('item 0:', items[0]);
-        });
-    },
 }
