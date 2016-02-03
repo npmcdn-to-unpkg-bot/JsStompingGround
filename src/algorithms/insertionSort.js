b53a83337306c5cfc46780cb4453490c6a13813a
daniel-williams <mail@danielwilliams.us>
2016-02-03 10:31:46 -0800

    Algorithms: Insertion Sort

--- src/algorithms/insertionSort.js
+++ src/algorithms/insertionSort.js
@@ -1,4 +1,6 @@
 import {log, generateRandomIntegers} from '../utils';
+import {swap} from './sortHelpers';
+
 
 log('Given an unsorted array of numbers, produce a sorted array.')
 let unsorted = generateRandomIntegers(20);
@@ -8,6 +10,14 @@ log('unsorted', unsorted.toString());
 log('sorted', insertionSort(sorted).toString());
 
 function insertionSort(a) {
-  
+  let i, j = 1;
+  while (j < a.length) {
+    for(i = j; i >= 0; i--) {
+      if(a[i] < a[i-1]) {
+        swap(a, i, i-1);
+      }
+    }
+    j++;
+  }
   return a;
 }
