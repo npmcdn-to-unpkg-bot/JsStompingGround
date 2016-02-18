180cc2b404e8566f90be417e6de1fad779e27b26
daniel-williams <mail@danielwilliams.us>
2016-02-17 19:05:56 -0800

    Data Structures: AvlTree updates

--- src/dataStructures/AvlTree.js
+++ src/dataStructures/AvlTree.js
@@ -10,7 +10,7 @@ function AvlTree(options) {
 }
 
 AvlTree.prototype.insert = function(key, value) {
-  let newNode = new Node(key, value, this.options);
+  let newNode = new Node(key, value, this);
   if(this.root === null) {
     this.root = newNode;
     return this.root;
@@ -21,6 +21,39 @@ AvlTree.prototype.insert = function(key, value) {
 AvlTree.prototype.contains = function(key) {
   return this.root === null ? false : this.root.contains(key);
 };
+AvlTree.prototype.delete = function(key) {
+  if(this.root === null) { return false; }
+  if(this.root.key === key) {
+    return this.deleteIfLeaf() || this.deleteIfSingleChild() || this.deleteIfTwoChildren();
+  } else {
+    this.root.delete(key);
+  }
+}
+
+AvlTree.prototype.deleteIfLeaf = function() {
+  if(this.root.left !== null || this.root.right !== null) { return false; }
+  // console.log(`root with zero children, deleting ${this.key}`);
+  this.root === null;
+  return true;
+}
+AvlTree.prototype.deleteIfSingleChild = function() {
+  if(this.root.left === null && this.root.right === null) { return false; }
+  if(this.root.left !== null && this.root.right !== null) { return false; }
+  // console.log(`root with single child, deleting ${this.key}`);
+  if(this.root.left !== null) {
+    this.root = this.root.left;
+  } else {
+    this.root = this.root.right;
+  }
+  this.root.calcHeight();
+  return true;
+}
+AvlTree.prototype.deleteIfTwoChildren = function() {
+  if(this.left === null || this.right === null) { return false; }
+  // console.log(`root with two children, deleting ${this.key}`);
+  console.log('Warn: deleteIfTwoChildren not implemented yet.');
+  return false;
+}
 
 AvlTree.prototype.traverseInOrder = function traverseInOrder(fn) {
   if(this.root !== null) {
@@ -57,11 +90,22 @@ AvlTree.prototype.toArray = function toArray() {
 AvlTree.prototype.toString = function toString() {
   return this.toArray().toString();
 };
+AvlTree.prototype.prettyPrint = function (printData = false, spacing = '') {
+  if(this.root === null) {
+    console.log('empty tree');
+  } else {
+    this.root.visualize();
+  }
+};
+
+AvlTree.prototype.isBst = function() {
+  if(this.root === null) { return false; }
+  return this.root.isBst();
+}
 
 
 
-
-function Node(key, value, options) {
+function Node(key, value, context) {
   if(typeof key !== 'number' && typeof key !== 'string') { throw Error('key is required to be a number or string.'); }
 
   this.key = key;
@@ -73,8 +117,9 @@ function Node(key, value, options) {
 
   this.height = 0;
 
-  this.options = options;
+  this.context = context;
 }
+
 
 Node.prototype.insert = function(newNode) {
   if(newNode.key < this.key) {
@@ -95,7 +140,7 @@ Node.prototype.insert = function(newNode) {
     return newNode;
   } else {
     // duplicate
-    if(this.options.unique) {
+    if(this.context.options.unique) {
       console.log(`Insert failed for ${newNode.key}. Duplicate detected.`)
     } else {
       console.log(`Insert failed for ${newNode.key}. Multiple values not yet implemented.`)
@@ -112,7 +157,85 @@ Node.prototype.contains = function(key) {
     return true;
   }
 }
+Node.prototype.delete = function(key) {
+  if(key < this.key) {
+    if(this.left !== null) {
+      this.left.delete(key);
+    }
+  } else if (key > this.key) {
+    if(this.right !== null) {
+      this.right.delete(key);
+    }
+  } else {
+    if(this.parent === null) { return this.context.delete(key); } // root node special case - tree needs to handle the delete
+    return this.deleteIfLeaf() || this.deleteIfSingleChild() || this.deleteIfTwoChildren();
+  }
+}
 
+Node.prototype.deleteIfLeaf = function() {
+  if(this.left !== null || this.right !== null) { return false; }
+  console.log(`deleting ${this.key}`);
+  console.log(`  leaf`);
+  if(this.parent.left === this) {
+    this.parent.left = null;
+  } else {
+    this.parent.right = null;
+  }
+  this.parent.traverseToRoot(this.parent.calcHeight);
+  return true;
+}
+Node.prototype.deleteIfSingleChild = function() {
+  if(this.left !== null && this.right !== null) { return false; }
+
+  let child = this.left || this.right;
+  console.log(`deleting ${this.key}`);
+  console.log(`  single child(${child.key})`);
+
+  if(this.parent.left === this) {
+    this.parent.left = child;
+  } else {
+    this.parent.right = child;
+  }
+  child.parent = this.parent;
+
+  this.parent.traverseToRoot(this.parent.calcHeight);
+  return true;
+}
+Node.prototype.deleteIfTwoChildren = function() {
+  if(this.left === null || this.right === null) { return false; }
+  console.log(`deleting ${this.key}`);
+  console.log(`  two children`);
+  // TODO: randomly shift left.getMaxDescendant (for balance)
+  const shift = this.right.getMinDescendant();
+  console.log(`  in order successor(${shift.key})`);
+  this.key = shift.key;
+  this.data = shift.data;
+
+  if(this === shift.parent) {
+    this.right = shift.right;
+  } else {
+    shift.parent.left = shift.right;
+  }
+  if(shift.right !== null) {
+    shift.right.parent = shift.parent;
+  }
+  return true;
+}
+
+Node.prototype.getMaxDescendant = function() {
+  if(this.right !== null) {
+    return this.right.getMaxDescendant();
+  } else {
+    return this;
+  }
+}
+Node.prototype.getMinDescendant = function() {
+  if(this.left !== null) {
+    return this.left.getMinDescendant();
+  } else {
+    return this;
+  }
+}
 Node.prototype.traverseToRoot = function traverseToRoot(fn) {
   if(typeof fn !== 'function') { return };
   fn.call(this);
@@ -149,13 +272,18 @@ Node.prototype.calcHeight = function calcHeight() {
   return this.height;
 };
 
+Node.prototype.spacer = '| ';
 Node.prototype.toArray = function toArray() {
   let a = [];
   this.traverseInOrder(function() {
+    let count = (this.left !== null ? 1 : 0) + (this.right !== null ? 1 : 0);
+    // if(this.left !== null) { count++; }
+    // if(this.right !== null) { count++; }
     a.push({
       key: this.key,
       parent: !!this.parent ? this.parent.key : '-',
       height: this.height,
+      children: count,
     });
   });
   return a;
@@ -163,6 +291,63 @@ Node.prototype.toArray = function toArray() {
 Node.prototype.toString = function toString() {
   return this.toArray().toString();
 };
+Node.prototype.prettyPrint = function (printData, spacing = '') {
 
+
+  let data = printData
+    ? ` (${this.data})`
+    : '';
+  console.log(`${spacing}${this.key}${data}`);
+
+  if (!this.left && !this.right) { return; }
+
+  if (this.left) {
+    this.left.prettyPrint(printData, spacing + this.spacer);
+  }
+  else {
+    console.log(`${spacing}${this.spacer}-`);
+  }
+  if (this.right) {
+    this.right.prettyPrint(printData, spacing + this.spacer);
+  } else {
+    console.log(`${spacing}${this.spacer}-`);
+  }
+};
+Node.prototype.visualize = function(prefix = '') {
+  console.log(`${prefix}${this.key}`);
+  prefix += this.spacer;
+  if(this.left === null && this.right === null) { return; }
+  if(this.left !== null) {
+    this.left.visualize(prefix);
+  } else {
+    console.log(`${prefix}-`);
+  }
+  if(this.right !== null) {
+    this.right.visualize(prefix);
+  } else {
+    console.log(`${prefix}-`);
+  }
+}
+
+
+Node.prototype.isBst = function() {
+  this.traversePreOrder(function() {
+    if(this.left !== null && this.left.key > this.key) {
+      throw Error(`BST check failed @ key(${this.key}). Left key(${this.left.key}) is larger.`);
+    }
+    if(this.right !== null && this.right.key < this.key) {
+      throw Error(`BST check failed @ key(${this.key}). Right key(${this.right.key}) is smaller.`);
+    }
+  });
+  this.traversePreOrder(function() {
+    if(this.left !== null && this.left.parent !== this) {
+      throw Error(`BST check failed @ key(${this.key}). Left key(${this.left.key}) parent pointer is not coherent.`);
+    }
+    if(this.right !== null && this.right.parent !== this) {
+      throw Error(`BST check failed @ key(${this.key}). Right key(${this.left.key}) parent pointer is not coherent.`);
+    }
+  });
+  return true;
+}
 
 export default AvlTree;
--- src/dataStructures/AvlTreeTest.js
+++ src/dataStructures/AvlTreeTest.js
@@ -1,8 +1,5 @@
-import {generateRandomIntegers} from '../utils';
+import {log, generateRandomIntegers} from '../utils';
 import AvlTree from './AvlTree';
-
-
-let log = console.log;
 
 let keys = generateRandomIntegers(20);
 log('random keys:', keys.toString());
@@ -10,14 +7,26 @@ log('random keys:', keys.toString());
 var avl = new AvlTree();
 keys.forEach((item, i) => avl.insert(item, i));
 
-let size = avl.size();``
-log(`Size of tree? ${size}`);
+log('');
+log('Is BST?', avl.isBst());
+log(`Size of tree? ${avl.size()}`);
 log(`Height of tree? ${avl.height()}`);
+log('');
+avl.prettyPrint();
+log('');
 
-let test = [keys[0], 1002, keys[10], keys[size -1]];
-test.forEach(item=>log(`Does tree contain ${item}? ${avl.contains(item)}`));
+log('');
+let deleteKeys = [keys[4], keys[14], keys[7], keys[6], keys[18], keys[5]];
+deleteKeys.forEach(key => {
+  avl.delete(key);
+});
 
-log('toArray: ', avl.toArray().sort((a, b) => {
-  let test = a.height > b.height ? -1 : a.height < b.height ? 1 : 0;
-  return test;
-}));
+log('');
+avl.prettyPrint();
+log('');
+log('Is BST?', avl.isBst());
+log(`Size of tree? ${avl.size()}`);
+log(`Height of tree? ${avl.height()}`);
+// log(avl.toArray().sort((a,b) => {
+//   return a.height < b.height ? -1 : a.height > b.height ? 1 : 0;
+// }).reverse());
