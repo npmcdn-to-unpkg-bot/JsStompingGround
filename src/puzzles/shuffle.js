9f286b0a82839bfc078d97689ff948f28306e596
daniel-williams <mail@danielwilliams.us>
2016-03-16 12:30:30 -0700

    Shuffle refactor.

--- src/puzzles/shuffle.js
+++ src/puzzles/shuffle.js
@@ -9,17 +9,16 @@ while(i < original.length) {
 const shuffled = shuffle(original.slice());
 
 log('original:', original.toString());
+log('');
 log('shuffled:', shuffled.toString());
 
 // we can take some queues from bubble sort to shuffle in place
 function shuffle(a) {
-  let i, r, j = a.length - 1;
+  let r, j = a.length - 1;
 
   while(j > 0) {
-    for(i = 0; i <= j; i++) {
-      r = Math.floor(Math.random() * j);
-      swap(a, i, r);
-    }
+    r = Math.round(Math.random() * j);
+    swap(a, r, j);
     j--;
   }
   return a;
