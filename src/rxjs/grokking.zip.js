f73afd403e341c397e45c576139d7739c4f1b8dd
daniel-williams <mail@danielwilliams.us>
2016-05-06 14:17:02 -0700

    Observable zip test

--- src/rxjs/grokking.zip.js
+++ src/rxjs/grokking.zip.js
@@ -4,9 +4,7 @@ import {log} from '../utils';
 let range$ = Observable.range(0, 5);
 let async$ = Observable.interval(1000);
 
-let c = Observable.zip(range$, async$, range$.skip(2));
-
-c.subscribe(
+range$.zip(async$, range$.skip(2)).subscribe(
   (x) => log(x),
   (err) => log('err:', err),
   () => log('complete')
